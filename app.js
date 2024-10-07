4675445896b19cb0ecdb594db0a88d20d391e6c6
Axolotlecoder <151933458+Axolotlecoder@users.noreply.github.com>
2024-10-07 08:42:12 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('Service Worker registriert:', registration);
+      }).catch((error) => {
+        console.log('Service Worker Registrierung fehlgeschlagen:', error);
+      });
+  });
+}
