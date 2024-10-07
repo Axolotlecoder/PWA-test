88d9283dfae5a7bb08470ae1c71772cdb933669c
Axolotlecoder <151933458+Axolotlecoder@users.noreply.github.com>
2024-10-07 08:41:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/icon-192.png',
+        '/icon-512.png',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
 
